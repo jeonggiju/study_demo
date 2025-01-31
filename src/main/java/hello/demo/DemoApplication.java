92111c4002ebd05e4f42939a791178c44399631b
jeonggiju <halogiju123@gmail.com>
2025-01-31 14:15:19 +0900

    2025/1/31

--- src/main/java/hello/demo/DemoApplication.java
+++ src/main/java/hello/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package hello.demo;
 
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		MemberService memberService = new MemberServiceImpl();
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		memberService.join(member);
+		
+		Member findMember = memberService.findMember(1L);
+		System.out.println("member.getName() = " + member.getName());
+		System.out.println("findMember.getName() = " + findMember.getName());
 	}
-
 }
---
+++ src/test/java/hello/demo/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.demo;
+
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+import hello.demo.order.Order;
+import hello.demo.order.OrderService;
+import hello.demo.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
